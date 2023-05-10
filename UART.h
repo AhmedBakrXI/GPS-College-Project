fec47a78a742ed90589984ce2e2950857cd37ded
MostafaQenawy <130760077+MostafaQenawy@users.noreply.github.com>
2023-05-10 20:39:11 +0300

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,1 +1,15 @@
+/*
+Mostafa hessin ahmed qenawy
+2000931
+*/
 
+#ifndef UART_H_
+#define UART_H_
+
+void UART5_init(void);           // Function to initialise UART5 and GPIO for PE4 and PE5
+void SEND_BYTE(char c);
+char RECEIVE_BYTE(void);
+void RECEIVE_STRING(char* str, int length);
+void SEND_STRING(char* str);
+
+#endif
